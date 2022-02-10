9c588977cf6d3a2ad0e89b9e2fa845b8f54a17ff
Ivan Fernandez Gracia <48660555+IvanFernandezGracia@users.noreply.github.com>
2022-02-10 01:27:53 -0300

    make it better

--- models/server.js
+++ models/server.js
@@ -37,7 +37,7 @@ class Server {
       ],
       handlePreflightRequest: (req, res) => {
         res.writeHead(200, {
-          "Access-Control-Allow-Origin": "http://localhost:3000",
+          "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET,POST",
           "Access-Control-Allow-Headers": "my-custom-header",
           "Access-Control-Allow-Credentials": true,
