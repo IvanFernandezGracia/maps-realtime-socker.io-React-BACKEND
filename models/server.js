b5077371c20c7d42c567890fab21f16681bd4a69
Ivan Fernandez Gracia <48660555+IvanFernandezGracia@users.noreply.github.com>
2022-02-10 02:02:48 -0300

    make it better

--- models/server.js
+++ models/server.js
@@ -34,7 +34,7 @@ class Server {
       origins: this.pathCors,
       handlePreflightRequest: (req, res) => {
         res.writeHead(200, {
-          "Access-Control-Allow-Origin": this.pathCors,
+          "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET,POST",
           "Access-Control-Allow-Headers": "my-custom-header",
           "Access-Control-Allow-Credentials": true,
